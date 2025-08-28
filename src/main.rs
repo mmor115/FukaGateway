1eb737797e2786e9012bf545dc0f69fd4c7a869d
Max Morris <mmor115@lsu.edu>
2025-08-28 21:14:02 +0000

    Working on landlock.

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,18 @@ mod job;
 
 use actix_web::{App, HttpServer};
 use std::io;
+use landlock::{path_beneath_rules, Access, AccessFs, AccessNet, NetPort, RulesetAttr, RulesetCreatedAttr, RulesetError, RulesetStatus};
+use crate::job::{get_jobs_dir, get_scripts_dir};
+
+const PORT: u16 = 8080;
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
+    if cfg!(target_os = "linux") {
+        let landlock_status = do_landlock().expect("Failed to enforce Landlock ruleset");
+        println!("Landlock status: {:?}", landlock_status);
+    }
+
     HttpServer::new(|| {
         App::new()
             .service(endpoints::echo)
@@ -17,7 +26,26 @@ async fn main() -> io::Result<()> {
             .service(endpoints::poll_job)
             .service(endpoints::download_job)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", PORT))?
     .run()
     .await
+}
+
+#[cfg(target_os = "linux")]
+fn do_landlock() -> Result<RulesetStatus, RulesetError> {
+    let abi = landlock::ABI::V6;
+    let ruleset =
+        landlock::Ruleset::default()
+                          .handle_access(AccessFs::from_all(abi))?
+                          .handle_access(AccessNet::BindTcp)?
+                          .handle_access(AccessNet::ConnectTcp)?;
+
+    let status =
+        ruleset.create()?.add_rules(path_beneath_rules(&[get_jobs_dir(), get_scripts_dir()], AccessFs::from_all(abi)))?
+                         .add_rules(path_beneath_rules(&[get_scripts_dir()], AccessFs::Execute))?
+                         .add_rules(path_beneath_rules(&["/usr/bin/sbatch", "/usr/bin/squeue"], AccessFs::Execute))?
+                         .add_rule(NetPort::new(PORT, AccessNet::BindTcp))?
+                         .restrict_self()?;
+
+    Ok(status.ruleset)
 }
